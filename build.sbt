3b792e01d1bb0cfcf32da1782a21d2d77d9da2b0
Alex Sales <asales@ewise.com>
2015-09-16 22:09:24 +0800

    added local integration test for dynamodb

--- build.sbt
+++ build.sbt
@@ -1,8 +1,22 @@
-name := """simple-rest-scala"""
+name := """car-advert"""
 
 version := "1.0-SNAPSHOT"
 
+scalacOptions ++= Seq("-encoding", "UTF-8")
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  specs2 % Test
+)
+
 lazy val root = project.in(file(".")).enablePlugins(PlayScala)
 
+
+libraryDependencies ++= Seq(
+  "com.github.seratch" %% "awscala" % "0.5.+",
+  "org.scalatest" %% "scalatest" % "2.2.1" % "test",
+  "org.scalatestplus" %% "play" % "1.1.0" % "test"
+)
 
 fork in run := true
---
+++ test/module/dao/DynamoDBSpec.scala
@@ -0,0 +1,42 @@
+package module.dao
+
+import awscala.dynamodbv2.DynamoDB
+import org.slf4j.LoggerFactory
+import org.specs2.mock.Mockito
+import org.specs2.mutable.Specification
+import awscala._, dynamodbv2._
+
+class DynamoDBSpec extends Specification with Mockito {
+  val log = LoggerFactory.getLogger(this.getClass)
+
+  implicit val dynamoDB = DynamoDB.local()
+
+  "DynamoDb " should {
+    "work" in {
+      val tableMeta: TableMeta = dynamoDB.createTable(
+        name = "Members",
+        hashPK =  "Id" -> AttributeType.Number,
+        rangePK = "Country" -> AttributeType.String,
+        otherAttributes = Seq("Company" -> AttributeType.String),
+        indexes = Seq(LocalSecondaryIndex(
+          name = "CompanyIndex",
+          keySchema = Seq(KeySchema("Id", KeyType.Hash), KeySchema("Company", KeyType.Range)),
+          projection = Projection(ProjectionType.Include, Seq("Company"))
+        ))
+      )
+
+      val table: Table = dynamoDB.table("Members").get
+
+      table.put(1, "Japan", "Name" -> "Alice", "Age" -> 23, "Company" -> "Google")
+      table.put(2, "U.S.",  "Name" -> "Bob",   "Age" -> 36, "Company" -> "Google")
+      table.put(3, "Japan", "Name" -> "Chris", "Age" -> 29, "Company" -> "Amazon")
+
+      val googlers: Seq[Item] = table.scan(Seq("Company" -> cond.gt("Google")))
+
+      table.destroy()
+
+      1 + 1 === 2
+    }
+  }
+
+}
