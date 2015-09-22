0b40d49ab8424fcf757dfba886db8a728cf38954
Alex Sales <asales@ewise.com>
2015-09-22 21:43:55 +0800

    refactored init creation of dao

--- app/module/dao/CarAdvertDao.scala
+++ app/module/dao/CarAdvertDao.scala
@@ -5,6 +5,7 @@ import com.google.inject.ImplementedBy
 import models.CarAdvert
 import models.CarAdvert
 import org.joda.time.DateTime
+import org.slf4j.LoggerFactory
 
 
 @ImplementedBy(classOf[CarAdvertDaoImpl])
@@ -28,116 +29,68 @@ class CarAdvertDaoImpl extends CarAdvertDao {
   val CAR_ADVERTS: String = "car-adverts"
   implicit val dynamoDb: DynamoDB = DynamoDB.local()
 
-  def createTable() = {
-    val tableMeta: TableMeta = dynamoDb.createTable(
-      name = CAR_ADVERTS, hashPK = "guid" -> AttributeType.String, rangePK = "title" -> AttributeType.String,
-    otherAttributes = Seq(),
-    indexes = Seq(LocalSecondaryIndex(
-      name = "car-adverts-index",
-      keySchema = Seq(
-        KeySchema("guid", KeyType.Hash),
-        KeySchema("title", KeyType.Range)
-      ),
-      projection = Projection(ProjectionType.Include, Seq("title"))
-    ))
-    )
-  }
+  createTableIfMissing()
 
-  override def save(carAdvert: CarAdvert): Unit = {
+  def createTableIfMissing() = {
     dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => {
-          saveOrUpdate(carAdvert, table)
-      }
       case None => {
-        createTable()
-        save(carAdvert)
+        val tableMeta: TableMeta = dynamoDb.createTable(
+          name = CAR_ADVERTS, hashPK = "guid" -> AttributeType.String, rangePK = "title" -> AttributeType.String,
+          otherAttributes = Seq(),
+          indexes = Seq(LocalSecondaryIndex(
+            name = "car-adverts-index",
+            keySchema = Seq(
+              KeySchema("guid", KeyType.Hash),
+              KeySchema("title", KeyType.Range)
+            ),
+            projection = Projection(ProjectionType.Include, Seq("title"))
+          ))
+        )
       }
     }
   }
 
-  override def findById(id: String): Option[CarAdvert] = {
-    dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => {
-        table.query(Seq("guid" -> cond.eq(id))) match {
-          case Nil => None
-          case items => {
-            Some(CarAdvert.toObject(items(0)))
-          }
-        }
-      }
-      case None => {
-        createTable()
-        findById(id)
-      }
+  override def save(carAdvert: CarAdvert): Unit = saveOrUpdate(carAdvert)
+
+  override def findById(id: String): Option[CarAdvert] = dynamoDb.table(CAR_ADVERTS).get.query(Seq("guid" -> cond.eq(id))) match {
+    case Nil => None
+    case items => {
+      Some(CarAdvert.toObject(items(0)))
     }
   }
 
   override def findAll(): List[CarAdvert] = {
-    dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => {
-        table.scan(Seq("guid" -> cond.ne("a"))) match {
-          case items => items.map(item => CarAdvert.toObject(item)).toList
-          case Nil => List()
-        }
-      }
-      case None => {
-        createTable()
-        findAll()
-      }
+    dynamoDb.table(CAR_ADVERTS).get.scan(Seq("guid" -> cond.ne("a"))) match {
+      case items => items.map(item => CarAdvert.toObject(item)).toList
+      case Nil => List()
     }
   }
-
-
-
 
   override def findAll(sortField: String): List[CarAdvert] = {
-    dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => {
-        table.scan(Seq("guid" -> cond.ne("a"))) match {
-          case items => items.map(CarAdvert.toObject(_)).toList.sorted(CarAdvert.getOrdering(sortField))
-          case Nil => List()
-        }
-      }
-      case None => {
-        createTable()
-        findAll(sortField)
-      }
+    dynamoDb.table(CAR_ADVERTS).get.scan(Seq("guid" -> cond.ne("a"))) match {
+      case items => items.map(CarAdvert.toObject(_)).toList.sorted(CarAdvert.getOrdering(sortField))
+      case Nil => List()
     }
   }
 
-  override def update(carAdvert: CarAdvert): Boolean = {
-    dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => {
-        findById(carAdvert.guid) match {
-          case item => saveOrUpdate(carAdvert, table); true
-          case None => false
-        }
-      }
-      case None => {
-        createTable()
-        update(carAdvert)
-      }
-    }
+  override def update(carAdvert: CarAdvert): Boolean = findById(carAdvert.guid) match {
+    case item => saveOrUpdate(carAdvert); true
+    case None => false
   }
 
   override def deleteBy(id: String, title: String): Boolean = {
-    dynamoDb.table(CAR_ADVERTS) match {
-      case Some(table) => table.delete(id, title); true
-      case None => {
-        createTable()
-        deleteBy(id, title)
-      }
-    }
+    dynamoDb.table(CAR_ADVERTS).get.delete(id, title);
+    true
   }
 
 
-  private def saveOrUpdate(carAdvert: CarAdvert, table: Table): Unit = {
+  private def saveOrUpdate(carAdvert: CarAdvert): Unit = {
     val firstRegistration = carAdvert.firstRegistration match {
       case Some(item) => new DateTime(item).withTimeAtStartOfDay().getMillis // without time.
       case None => None
     }
 
-    table.put(
+    dynamoDb.table(CAR_ADVERTS).get.put(
       hashPK = carAdvert.guid,
       rangePK = carAdvert.title,
       "fuel" -> carAdvert.fuel,
